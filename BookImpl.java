b7e22a89a472f6a4c90abd00ee72b785152a977a
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-01-18 18:33:49 +0000

    First commit

---
+++ BookImpl.java
@@ -0,0 +1,19 @@
+public class BookImpl implements Book {
+	
+	private String author;
+	private String title;
+	
+	public BookImpl(String author, String title) {
+		this.author = author;
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return null;
+	}
+	
+	public String getTitle() {
+		return null;
+	}
+	
+}
